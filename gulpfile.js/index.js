3b419af33faed58115d2a5df0e47db1815ce441d
pomelott <pomelott@163.com>
2020-05-23 21:32:01 +0800

    optimize minify config

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,39 +3,42 @@ const {
     src,
     dest,
     task
-  } = require('gulp');
-  const imagemin = require('gulp-imagemin');
-  const smushit = require('gulp-smushit');
-  const tinypng_nokey = require('gulp-tinypng-nokey');
-  task('minImg', function () {
+} = require('gulp');
+const imagemin = require('gulp-imagemin');
+const smushit = require('gulp-smushit');
+const tinypng_nokey = require('gulp-tinypng-nokey');
+// 通过使用缓存提高压缩速度
+const cache = require('gulp-cache');
+task('minImg', function () {
     return src('dist/images/**/*.*')
         // 脚本压缩
-        // .pipe(imagemin([
-        //     // imagemin.gifsicle({interlaced: true}),
-        //     imagemin.mozjpeg({quality: 75, progressive: true}),
-        //     imagemin.optipng({optimizationLevel: 5}),
-        //     imagemin.svgo({
-        //         plugins: [
-        //             {removeViewBox: true},
-        //             {cleanupIDs: false}
-        //         ]
-        //     })
-        // ]))
+        // .pipe(
+        //     cache(
+        //         imagemin([
+        //             // imagemin.gifsicle({interlaced: true}),
+        //             imagemin.mozjpeg({quality: 75, progressive: true}),
+        //             imagemin.optipng({optimizationLevel: 5}),
+        //             imagemin.svgo({
+        //                 plugins: [
+        //                     {removeViewBox: true},
+        //                     {cleanupIDs: false}
+        //                 ]
+        //             })
+        //         ])
+        //     )
+        // )
         // 在线压缩，压缩率较高但需要翻墙且图片格式有限制
-        // .pipe(smushit({
-        //     verbose: true
-        // }))
+        // .pipe(
+        //     cache(
+        //         smushit({
+        //             verbose: true
+        //         })
+        //     )
+        // )
         // 在线压缩
-        .pipe(tinypng_nokey())
+        .pipe(cache(tinypng_nokey()))
         .pipe(dest('dist/images'))
-  
-  })
-  
-  // function minImg (cb) {
-  //   src('../build/static/images/bg.png')
-  //         .pipe(imageMin({progressive: true}))
-  //         .pipe(dest('../build/dest/images'))
-  //   Promise.resolve(true);
-  // }
-  
-  task('minImg')
+
+})
+
+task('minImg')
